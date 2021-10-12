b5f229b2418748851aa97433a21a5ce5a23f3a3d
Andrea <andrea@zerotredici.com>
2021-10-12 10:29:36 +0200

    [ADD] Create and edit text file through filesystem module

---
+++ note_app/app.js
@@ -0,0 +1,15 @@
+/**
+ * if we are working or exporting a module we can use the syntax import {function_A, function_B, ..., function_n} from 'module_name'
+ * In this case:
+ * 		import {writeFileSync, appendFileSync} from 'fs'
+ * and then use it as a single function without the prefix with the const name es:
+ *  writeFileSync('filename', 'hello, world!')
+*/
+const fs = require('fs')
+
+try {
+	fs.writeFileSync('notes.txt', 'File created with Node.js')
+	fs.appendFileSync('notes.txt', ' - Text added later with "appendFileSync"')
+} catch (error) {
+	console.log(error)	
+}
