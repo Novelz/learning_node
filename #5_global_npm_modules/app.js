3a8c8f48b462c8b1f037f96c143068827022b366
Andrea <andrea@zerotredici.com>
2021-10-12 17:41:12 +0200

    [L5] Npm global package - Nodemon

---
+++ #5_global_npm_modules/app.js
@@ -0,0 +1,17 @@
+/**
+ * To install and use nodemon we need to install it with a slightly different command
+ * > npm install nodemon -g
+ * 
+ * The -g flag stands for "global" and the package will be installed on the OS
+ * As you can see there isn't any package or package-lock file
+ * 
+ * We can then simply use the nodemon module in the terminal with the nodemon command
+ */
+
+
+
+// write nodemon app.js in the terminal and check the output
+console.log('Testing nodemon module')
+
+//uncomment the following line, save and check the terminal output
+console.log('Testing nodemon module updating')
