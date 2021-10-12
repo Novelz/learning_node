1f5bc733654b88d0e3abfe9c95025116f113de87
Andrea <andrea@zerotredici.com>
2021-10-12 11:09:49 +0200

    [L2] Import personal module

--- note_app/app.js
+++ note_app/app.js
@@ -1,15 +1,7 @@
 /**
- * if we are working or exporting a module we can use the syntax import {function_A, function_B, ..., function_n} from 'module_name'
- * In this case:
- * 		import {writeFileSync, appendFileSync} from 'fs'
- * and then use it as a single function without the prefix with the const name es:
- *  writeFileSync('filename', 'hello, world!')
-*/
-const fs = require('fs')
+ * Require a personal module that export just 1 function
+ * and use it in this main file
+ */
+const getNotes = require('./notes.js')
 
-try {
-	fs.writeFileSync('notes.txt', 'File created with Node.js')
-	fs.appendFileSync('notes.txt', ' - Text added later with "appendFileSync"')
-} catch (error) {
-	console.log(error)	
-}
+console.log(getNotes())
