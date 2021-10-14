3743e4ba6d075dbf0f00a66eac4a3d3c8859aff7
Andrea <andrea.novella.wd@gmail.com>
2021-10-14 11:32:43 +0200

    [L8] Save date in JSON file

--- #8_store_data/app.js
+++ #8_store_data/app.js
@@ -1,5 +1,25 @@
+/**
+ * # Store date in a JSON file
+ * 
+ * We are going to take the data submitted by the user and save it in JSON file
+ * @param {String} name -> the name of the user
+ * 
+ * Terminal commands:
+ * > node app.js --name="INSERT_NAME"
+ * 
+ */
+const fs = require('fs')
 const yargs = require('yargs')
 
-yargs.version('2.0.0')
+const args = yargs.argv
+const userName = args.name
+
+try {
+	const userJSON = JSON.stringify({ "name": userName })
+	fs.writeFileSync('data.json', userJSON)
+	console.log('Username saved')
+} catch (error) {
+	console.error('Username not saved!')
+}
 
 yargs.parse()
