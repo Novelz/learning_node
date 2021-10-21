ad0ff6ac74f2d6c695b3a5853cbb2de7cccadbdf
Andrea <andrea.novella.wd@gmail.com>
2021-10-21 17:49:06 +0200

    Code refactor weather app

--- weather_app/app.js
+++ weather_app/app.js
@@ -17,43 +17,48 @@ const map_apikey = config.map_apikey
 const place_lat = '45.4299'
 const place_lng = '10.98444'
 
-/*
-const weatherstackUrl = "http://api.weatherstack.com/current?access_key=" + weather_apikey +"&query="+place_lat+","+place_lng+"&units="+config.units
-request({url: weatherstackUrl, json: true}, (error, response) => {
-	if(error) {
-		console.log('An error occurred while fetching the weather info!')
-		return false
-	}
+const geocode = (address, callback) => {
+	const mapboxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+ encodedURIComponent(address) +".json?access_token=" + map_apikey + "&limit=1"
 
-	if(response.body.error) {
-		console.log(response.body.error.info)
-		return false
-	}
+	request({ url: mapboxUrl, json: true }, (error, response) => {
+		//OS errors
+		if (error) {
+			console.error('An error occurred while fetching location info!')
+			return false
+		}
 
-	const currentWeather = response.body.current
-	console.log(currentWeather.weather_descriptions[0] + ". It's currently " + currentWeather.temperature + " degrees out. Humidity is at " + currentWeather.humidity +"%.")
-})
-*/
+		if (response.body.message) {
+			console.log(response.body.message)
+			return false
+		}
 
-const mapboxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/rome.json?access_token="+map_apikey+"&limit=1"
+		if (response.body.features.length == 0) {
+			console.log('Unable to find location. Try another search.')
+			return false
+		}
 
-request({url: mapboxUrl, json: true}, (error, response) => {
-	//OS errors
-	if(error) {
-		console.error('An error occurred while fetching location info!')
-		return false
-	}
+		const locationInfo = response.body.features[0] //keep only the first result
+		callback({lat: locationInfo.center[1], lng: locationInfo.center[0]})
+	})
+}
 
-	if(response.body.message) {
-		console.log(response.body.message)
-		return false
-	}
+const weatherForecast = (latlngInfo) => {
+	const weatherstackUrl = "http://api.weatherstack.com/current?access_key=" + weather_apikey + "&query=" + latlngInfo.lat + "," + latlngInfo.lng + "&units=" + config.units
+	
+	request({ url: weatherstackUrl, json: true }, (error, response) => {
+		if (error) {
+			console.log('An error occurred while fetching the weather info!')
+			return false
+		}
 
-	if(response.body.features.length == 0) {
-		console.log('Unable to find location. Try another search.')
-		return false
-	}
+		if (response.body.error) {
+			console.log(response.body.error.info)
+			return false
+		}
 
-	const locationInfo = response.body.features[0] //keep only the first result
-	console.log("Lat: "+locationInfo.center[1]+", Lng: "+locationInfo.center[0])
-})
+		const currentWeather = response.body.current
+		console.log(currentWeather.weather_descriptions[0] + ". It's currently " + currentWeather.temperature + " degrees out. Humidity is at " + currentWeather.humidity + "%.")
+	})
+}
+
+geocode('verona', (latlng) => { weatherForecast(latlng) })
