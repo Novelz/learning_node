25a7042464b719023e9651cca64c3d2bf290c541
Andrea <andrea.novella.wd@gmail.com>
2021-10-22 11:07:45 +0200

    [UPD] Use user input to find weather forecast and geographic informations

--- weather_app/app.js
+++ weather_app/app.js
@@ -10,7 +10,11 @@
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-geocode('rome', (error, geoData) => {
+let address = process.argv[2]
+
+if(!address) return console.log('Missing location.')
+
+geocode(address, (error, geoData) => {
 	if (error) return console.log('Error', error)
 
 	forecast(geoData.lat, geoData.lng, (error, forecastData) => {
